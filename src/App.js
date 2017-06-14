f69dc69ab40e78971c7d1a472fe233fa2f74f412
Raul <rauler7@gmail.com>
2017-06-14 14:46:30 -0600

    Firt commit Test App

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+var ReactDOM = require('react-dom')
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: '',
+      loading: true
+    };
+  }
+  
+  componentDidMount () {
+    const url = 'https://snacks-market-api-test.herokuapp.com/products';
+    
+    // in axios access data with .data
+    axios.get(url)
+      .then(response => {
+        this.setState({
+          data: response.data,
+          loading: false
+        });
+      })      
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
+  
+  render () {
+    let content;
+
+    if (this.state.loading) {
+      content = <div>Loading...</div>;
+    } else {      
+      content = this.state.data.map((snacks, index) => {
+        return (        
+          <tr key={index}>           
+            <td className="first-size">{snacks.name}</td>
+            <td className="second-size">{snacks.price}</td>
+            <td className="third-size">{snacks.stock}</td>              
+          </tr>
+        );
+      });
+    }
+
+    return (
+      <tr>
+        {content}
+      </tr>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />,  document.getElementById('root'));
